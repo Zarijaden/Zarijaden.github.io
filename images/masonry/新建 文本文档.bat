5143f683684a6593153a47df3ff1e9199eb763de
Zarijaden <Zarijaden@users.noreply.github.com>
2026-09-04 19:17:34 +0000

    deploy: 7223208ea7fc52011261a00657c08814a99bf8f3

---
+++ images/masonry/新建 文本文档.bat
@@ -0,0 +1,14 @@
+@echo off
+chcp 65001 >nul
+echo Generating masonry.yml...
+
+echo # Masonry gallery configuration > masonry.yml
+for %%i in (*.jpg *.png *.jpeg *.webp) do (
+  echo - image: /images/masonry/%%i >> masonry.yml
+  echo   exif: >> masonry.yml
+  echo   title: >> masonry.yml
+  echo   description: >> masonry.yml
+)
+
+echo Done! Check masonry.yml in current directory.
+pause
